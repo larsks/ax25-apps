4c8bbc197653cd1ae37d1b9d80b62878979a2d33
Thomas Osterried <thomas@osterried.de>
2009-04-22 21:22:01 +0000

    Added some includes. This fixes compilation problems in gentoo, 	and warnings.

--- ax25ipd/ax25ipd.h
+++ ax25ipd/ax25ipd.h
@@ -58,6 +58,7 @@
 #include	<stdlib.h>
 #include	<string.h>
 #include	<unistd.h>
+#include	<limits.h>
 #include	<arpa/inet.h>
 #include	<netinet/in.h>
 #include	<netinet/in_systm.h>
--- ax25ipd/bpqether.c
+++ ax25ipd/bpqether.c
@@ -1,4 +1,4 @@
-/* @(#) $Id: bpqether.c,v 1.4 2008/12/21 10:19:02 dl9sau Exp $ */
+/* @(#) $Id: bpqether.c,v 1.5 2009/04/22 21:22:01 dl9sau Exp $ */
 
 extern int ttyfd;
 /*
@@ -17,7 +17,6 @@ extern int ttyfd;
 #include <sys/socket.h>
 #include <sys/stat.h>
 #include <unistd.h>
-#include <limits.h>
 #include <syslog.h>
 
 #ifdef  linux
