larsks/ax25-apps

commit fb9187cbfcbe904f8d7dfe68a14d5e7428ad2684

ax25ipd: Switch version number printed to ax25-apps version.
The version number has not been changed ever since ax25-apps 0.0.1 making
the version useless for bug reporting.

Signed-off-by: Ralf Baechle <ralf@linux-mips.org>

ax25ipd/ax25ipd.c
@@ -172,7 +172,7 @@ int main(int argc, char **argv)
 
 void greet_world(void)
 {
-	printf("\nax25ipd %s / %s\n", VERS2, VERSION);
+	printf("\nax25ipd %s / %s\n", VERSION);
 	printf
 	    ("Copyright 1991, Michael Westerhof, Sun Microsystems, Inc.\n");
 	printf

ax25ipd/ax25ipd.h
@@ -37,8 +37,6 @@
  *
  */
 
-#define VERS2 "Version 1.0.2"
-
 #define IPPROTO_AX25 93
 #define DEFAULT_UDP_PORT 10093
 

commit 7243352849ef3f4947d4c25a2a05c1af7151564b

ax25ipd: Fix printing of version number.
fb9187cbfcbe ("ax25ipd: Switch version number printed to ax25-apps
version.") removed the VERS2 argument from printf but forgot to remove the
%s format string as well potencially crashing or printing garbage when
printing the version number.

Fixes: fb9187cbfcbe ("ax25ipd: Switch version number printed to ax25-apps version.")
Signed-off-by: Ralf Baechle <ralf@linux-mips.org>

ax25ipd/ax25ipd.c
@@ -66,7 +66,7 @@ static struct option options[] = {
 
 static void greet_world(void)
 {
-	printf("\nax25ipd %s / %s\n", VERSION);
+	printf("\nax25ipd %s\n", VERSION);
 	printf
 	    ("Copyright 1991, Michael Westerhof, Sun Microsystems, Inc.\n");
 	printf